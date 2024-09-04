c472e00ecf7a9c3c2f2446754674f30be714bb4b
sagarrao1 <sagarrao1@gmail.com>
2024-09-04 08:12:21 +0530

    Flink practice

---
+++ FlinkBasics/src/main/java/functional/LeftJoinNameAndLocation.java
@@ -0,0 +1,19 @@
+package functional;
+
+import org.apache.flink.api.common.functions.JoinFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.tuple.Tuple3;
+
+public class LeftJoinNameAndLocation implements JoinFunction<Tuple2<Integer, String>, Tuple2<Integer, String>, Tuple3<Integer, String, String>> {
+
+	@Override
+	public Tuple3<Integer, String, String> join(Tuple2<Integer, String> person, Tuple2<Integer, String> loc) {
+		
+		if(loc == null) {
+			return new Tuple3<>(person.f0,person.f1, "NULL");
+		}
+		return new Tuple3<>(person.f0,person.f1, loc.f1);		
+	}
+
+
+}
